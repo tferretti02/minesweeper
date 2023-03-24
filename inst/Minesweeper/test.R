e5143bd7a51f596377224c7f15a0d020b45d3f99
ThibaultF02 <thibaultferretti@gmail.com>
2023-03-24 13:39:14 +0100

    Update test.R

--- inst/Minesweeper/test.R
+++ inst/Minesweeper/test.R
@@ -1,54 +1,81 @@
 library(shiny)
+library(shinyWidgets)
+library(tidyr)
+library(dplyr)
+library(stringr)
 
+grid_rows <- 8
+grid_cols <- 10
+n_mine <- 10
+
+library(shiny)
+library(shinyWidgets)
+library(tidyr)
+library(dplyr)
+library(stringr)
+
+grid_rows <- 8
+grid_cols <- 10
+n_mine <- 10
+
+count_mines <- function(grid, row, col) {
+  r_start <- max(row - 1, 1)
+  r_end <- min(row + 1, nrow(grid))
+  c_start <- max(col - 1, 1)
+  c_end <- min(col + 1, ncol(grid))
+  sum(grid[r_start:r_end, c_start:c_end] == 1) - as.numeric(grid[row, col] == 1)
+}
+
+genarate_minesweeper_grid <- function(grid_rows, grid_cols, n_mine) {
+  mineland <- tidyr::expand_grid(row = 1:grid_rows, col = 1:grid_cols) %>%
+    mutate(
+      id = str_c("id", row, col),
+      mine = sample(c(rep(1, n_mine), rep(0, grid_rows * grid_cols - n_mine)))
+    )
+  mine_matrix <- matrix(mineland$mine, nrow = grid_rows, ncol = grid_cols, byrow = TRUE)
+  mineland$mine_count <- mapply(count_mines, grid = list(mine_matrix), row = mineland$row, col = mineland$col)
+  mineland
+}
+
+mineland_grid <- genarate_minesweeper_grid(grid_rows, grid_cols, n_mine)
+
+# Define UI for application that draws a histogram
 ui <- fluidPage(
-  fluidRow(
-    tagList(
-      tags$script(
-        "$(function(){
-        $(this).bind('contextmenu', function (e) {
-          e.preventDefault()
-          Shiny.setInputValue('plop', Math.random());
-        });
-      });"
-      )
-    ),
-    column(width = 3,
-           radioButtons("radio", "Choose a color:",
-                        choices = list("Red" = "red",
-                                       "Green" = "green",
-                                       "Blue" = "blue"))),
-    column(width = 9,
-           plotOutput("plot", click = "plot_click"))
-  )
+  
+  # Add custom CSS style for buttons
+  tags$head(tags$style(HTML("
+        .btn {
+            width: 50px;
+            height: 50px;
+            font-size: 14px;
+        }
+    "))),
+  
+  # Application title
+  tags$h1("Minesweeper"),
+  
+  # Sidebar with a slider input for number of bins
+  uiOutput("mineland")
 )
 
-server <- function(input, output, session) {
+# Define server logic required to draw a histogram
+server <- function(input, output) {
   
-  output$plot <- renderPlot({
-    plot(1:10, 1:10, col = input$radio)
-  })
-  
-  observeEvent(input$plot_click, {
-    click_loc <- input$plot_click
-    print(paste("Clicked at", click_loc$x, click_loc$y))
-  })
-  
-  observeEvent(input$plop , {
-    print(paste("Clicked at", input$plop))
-  })
-  
-  outputOptions(output, "plot", suspendWhenHidden = FALSE)
-  
-  output$script <- renderUI({
-    tags$script(
-      "$(function(){
-        $(this).bind('contextmenu', function (e) {
-          e.preventDefault()
-          Shiny.setInputValue('plop', Math.random());
-        });
-      });"
+  output$mineland <- renderUI({
+    do.call(
+      tagList,
+      lapply(1:grid_rows, function(ind) {
+        tmp <- mineland_grid %>% dplyr::filter(row == ind)
+        fluidRow(
+          actionGroupButtons(
+            inputIds = tmp$id,
+            labels = ifelse(tmp$mine == 1, "💣", as.character(tmp$mine_count))
+          )
+        )
+      })
     )
   })
 }
 
-shinyApp(ui, server)
+# Run the application
+shinyApp(ui = ui, server = server)
